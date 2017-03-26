b2fda17f08ba834530f5e5c8596b9001bfdfe489
Bilge <oncekenbilge@gmail.com>
2017-03-26 09:05:14 +0300

    Updated events/form.py I've learned how to modify form properties better(?)

--- events/forms.py
+++ events/forms.py
@@ -4,7 +4,9 @@ from . import models
 
 class ParkourEventForm(forms.ModelForm):
 
-    date = forms.DateField(widget=forms.DateInput(attrs={"class": "datepicker form-control"}))
+    ### I defined a dictonary instead of this
+    ### but i'll keep the line as a reference
+    # date = forms.DateField(widget=forms.DateInput(attrs={"class": "datepicker form-control"}))
 
     class Meta:
         model = models.ParkourEvent
@@ -14,3 +16,17 @@ class ParkourEventForm(forms.ModelForm):
             "date",
             "time",
         ]
+
+        # labels = {
+        #     "time":"Zaman"
+        # }
+
+        placeholders = {
+            "time": "H:M"
+        }
+
+        widgets = {
+                # ## It may not be the correct approach to do attrs in here
+                # ## But is definately easier at this moments. Check parkour_create.html
+            "date": forms.DateInput(attrs={"class": "datepicker form-control"})
+        }
