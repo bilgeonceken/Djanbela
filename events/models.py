f824e1f7eae58506a13b5125fee61c21f60af0fb
bilgeonceken <bilgeonceken@users.noreply.github.com>
2017-04-03 18:22:05 +0300

    Update models.py

--- events/models.py
+++ events/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
-# Create your models here.
-#
+
+
 class Event(models.Model):
     title = models.CharField(max_length=50)
     date = models.DateField(null=True)
